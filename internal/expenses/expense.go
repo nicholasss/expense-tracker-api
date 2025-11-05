f4bcf5ed9e68e64da0b569a7f67624d35868c03a
Nicholas <nicholasss@users.noreply.github.com>
2025-11-05 17:06:05 -0500

    fix: removed struct tags, due to not using these as request/response

--- internal/expenses/expense.go
+++ internal/expenses/expense.go
@@ -6,15 +6,15 @@ import "time"
 //
 // ID & RecordCreatedAt is set in the repository layer
 type Expense struct {
-	ID               int       `json:"id"`          // id of the expense for db
-	Amount           int64     `json:"amount"`      // cents total
-	ExpenseOccuredAt time.Time `json:"occuredAt"`   // when it happened
-	RecordCreatedAt  time.Time `json:"createdAt"`   // when the record was created
-	Description      string    `json:"description"` // what the transaction is
+	ID               int       // id of the expense for db
+	Amount           int64     // cents total
+	ExpenseOccuredAt time.Time // when it happened
+	RecordCreatedAt  time.Time // when the record was created
+	Description      string    // what the transaction is
 }
 
 // ExpenseSummary is used when the a summary is requested
 type ExpenseSummary struct {
-	SummaryTimeRange string `json:"summaryTimeRange"` // what time range
-	Total            int64  `json:"total"`            // cents total
+	SummaryTimeRange string // what time range
+	Total            int64  // cents total
 }
